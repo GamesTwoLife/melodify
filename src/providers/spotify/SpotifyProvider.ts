c62c1ff5598fdd25215a91e9862dad51ea0cf836
GamesTwoLife <vova08059910@gmail.com>
2025-02-17 10:22:47 +0200

    v1.0.13-dev

--- src/providers/spotify/SpotifyProvider.ts
+++ src/providers/spotify/SpotifyProvider.ts
@@ -19,13 +19,13 @@ export class SpotifyProvider implements IProvider {
 		try {
 			const response = await axios.post(
 				'https://accounts.spotify.com/api/token',
+				new URLSearchParams({
+					grant_type: 'client_credentials'
+				}).toString(),
 				{
 					headers: {
 						'Authorization': 'Basic ' + Buffer.from(`${this.clientId}:${this.clientSecret}`).toString('base64'),
 						'Content-Type': 'application/x-www-form-urlencoded'
-					},
-					params: {
-						grant_type: 'client_credentials'
 					}
 				}
 			);
@@ -47,7 +47,7 @@ export class SpotifyProvider implements IProvider {
 		const token = await this.getSpotifyAuth()
 
 		try {
-			const response = await axios.post(
+			const response = await axios.get(
 				'https://api.spotify.com/v1/search',
 				{
 					headers: { 'Authorization': `Bearer ${token}` },
@@ -72,7 +72,7 @@ export class SpotifyProvider implements IProvider {
 		const token = await this.getSpotifyAuth()
 
 		try {
-			const response = await axios.post(
+			const response = await axios.get(
 				`https://api.spotify.com/v1/tracks/${id}`,
 				{
 					headers: {
